171d114449ae52feb8cbb562c87e9f4d7f50ecac
datob <datoberidze2@yandex.ru>
2018-11-11 16:37:59 +0300

    finish modul1

--- modul.r
+++ modul.r
@@ -6,15 +6,14 @@ library(dplyr)
 beaver <- rbind(data.frame(beaver1, id = "beaver1"), 
                 data.frame(beaver2, id = "beaver2"))       
 #а)Температура тела от времени для 2 груп(активный и не активный)
-ggplot(beaver,  aes(x = time, y = temp,  colour = id)) + geom_point() + facet_grid(activ ~ .)
+ggplot(beaver1,  aes(x = time, y = temp, group=activ, colour =activ)) + geom_line() 
 #б)Гистограмма температуры
-qplot(temp, data = beaver,geom = "histogram", color=activ , facets = activ ~ .)
+qplot(temp, data = beaver1,geom = "histogram",group=activ, color=activ)
 #с)Среднее температура
-avg <- (mean(beaver$temp))
-qplot(avg, data = beaver,geom = "bar", color=activ,facets=activ~.) 
+beaver1 %>% group_by(activ) %>% summarise(avg=mean(temp)) %>% ggplot(aes(x=activ,y=avg)) + geom_point()mtcars
 #d)Яшики с усамы 
-beaver$activ<-ifelse(beaver$activ==1,"Active","Dormant")
-qplot(data=beaver,x=activ,y=temp,geom="boxplot", colour=activ)+
+beaver1$activ<-ifelse(beaver1$activ==1,"Active","Dormant")
+qplot(data=beaver1,x=activ,y=temp,geom="boxplot", colour=activ)+
    labs(title = "Boxplot of Temperatures",x = "Beaver State",y="Temperature",colour="State")
 
 
@@ -29,25 +28,16 @@ pie(cil, cex = 0.6, radius = 0.9, init.angle = -10,
     main = "Cylinders",
     col = c(2:8))
 #c) automatic manual transsmision
-cyl.am <- ggplot(mtcars, aes(x = factor(cyl), fill = factor(am)))
-cyl.am + geom_bar(position = "stack")
-cyl.am + geom_bar(position = "fill")
-cyl.am + geom_bar(position = "dodge")
-val = c("red", "green")
-lab = c("Manual", "Automatic")
-cyl.am + geom_bar(position = "dodge") + scale_x_discrete("Cylinders") + scale_y_continuous("Number") + scale_fill_manual("Transmission", values = val, labels = lab)
+mtcars %>% group_by(am) %>% summarise(avg=mean(mpg)) %>% ggplot(aes(x=am,y=avg)) + geom_bar(stat="identity", fill=c("green","red"))
 #d) Correl Matrix
-d <- data.frame(x1=rnorm(10),
-                 x2=rnorm(10),
-                 x3=rnorm(10))
-M <- cor(d) # get correlations
+M <- cor(mtcars) # get correlations
 library('corrplot') #package corrplot
 corrplot(M, method = "circle") #plot matrix
 
 
 #3 Задание 
 #price/carat
-ggplot(diamonds, aes(price,carat,color=cut)) +
+ggplot(diamonds, aes(carat,price,color=cut)) +
   geom_point() +
   geom_smooth()
 
